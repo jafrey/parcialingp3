c7aeaf6ec5ef4aa3c5addd9575c3c645006bdf41
Julian Araneda <julianaraneda97@gmail.com>
2019-11-12 13:10:54 -0300

    registrada nuevamente la app en settings, creado nuevamente el modelo cancion y registrado en admin.py

--- API/app/models.py
+++ API/app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class cancion(models.Model):
+    nom_cancion = models.CharField(max_length=30)
+    desc_cancion = models.CharField(max_length=30)
+    anio_salida = models.IntegerField()
+
+    class Meta:
+        verbose_name = "Cancion"
+        verbose_name_plural = "Canciones"
+
+    def __str__(self):
+        return self.nom_cancion
