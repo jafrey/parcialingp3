764b57cba83a6a0a8211d45b84af8f8cc51a3546
Julian Araneda <julianaraneda97@gmail.com>
2019-11-12 13:01:48 -0300

    Tuve que hacer todo de nuevo por que como un boludo no cambie el pull y seguia usando python 2.7 en vez de python 3, que era la idea, obviamente al levantar todo con python 3 me daba error por que toda la estructura estaba hecha con python 2.7

--- API/app/admin.py
+++ API/app/admin.py
@@ -1,10 +1,3 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.contrib import admin
-
-from models import cancion
-
-admin.site.register(cancion)
 
 # Register your models here.
--- API/app/models.py
+++ API/app/models.py
@@ -1,18 +1,3 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.db import models
 
 # Create your models here.
-
-class cancion(models.Model):
-    nom_cancion = models.CharField(max_length=30)
-    desc_cancion = models.CharField(max_length=30)
-    anio_salida = models.IntegerField()
-
-    class Meta:
-        verbose_name = "Cancion"
-        verbose_name_plural = "Canciones"
-
-    def __str__(self):
-        return self.nom_cancion
